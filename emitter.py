27654a534548750d4b729c1c5b30e905ebb635aa
KarinaHouska <62425032+KarinaHouska@users.noreply.github.com>
2020-03-28 15:58:20 +0100

    emitter input & encryption

--- emitter.py
+++ emitter.py
@@ -1,14 +1,16 @@
-from sympy import prime, factorint, mod_inverse
-
 class Emitter:
     def __init__(self):
         self.__message = 0
         self.encrypted_message = 0
 
     def write_message(self):
-        self.__message = 1 #input('Enter your last name: ')
-        pass
+        self.__message = input('create password for the bomb: ') # muss kleiner sein als n
+        # evtl. Test?:
+        # if (self.__message <= receiver.public_key.n - 1):
+        #   print('choose a shorter password.')
+        #   write_message()
+
 
     def encrypt_message(self, receiver):
-        self.__encrypted_message = self.__message, receiver.public_key.__e, receiver.public_key.__n
+        self.__encrypted_message = self.__message ** receiver.public_key.e % receiver.public_key.n # e und n sollten doch gerade nicht private sein...
         pass
